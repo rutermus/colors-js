9ba238533caf12299a3b5284bdd3967501c0134d
termus <rutermus@gmail.com>
2023-03-04 23:18:13 +0300

    added basic logic for the columns style

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
-console.log('working');
+const columns = document.querySelectorAll('.column');
+const colors = [];
+const hoverText = document.querySelectorAll('.hover-text');
+const hoverButtons = document.querySelectorAll('.hover-button');
+
+const setTextColor = (text, color) => {
+  const luminance = chroma(color).luminance();
+  const newColor =
+    luminance > 0.5 ? chroma(color).darken(2) : chroma(color).brighten(2);
+  text.style.color = newColor;
+};
+
+const setColumnsColor = () => {
+  columns.forEach((column) => {
+    const hexColor = chroma.random().toString().toUpperCase();
+
+    const columnHeader = column.querySelector('h2');
+    const lockButton = column.querySelector('button');
+
+    colors.push(hexColor);
+    columnHeader.textContent = hexColor;
+    column.style.background = hexColor;
+    setTextColor(columnHeader, hexColor);
+    setTextColor(lockButton, hexColor);
+  });
+};
+
+const setHoverProperties = (items) => {
+  for (let i = 0; i < items.length; i += 1) {
+    const luminance = chroma(colors[i]).luminance();
+    const currentBgColor = items[i].style.backgroundColor;
+    const currentItemColor = items[i].style.color;
+
+    items[i].addEventListener('mouseover', () => {
+      items[i].style.backgroundColor =
+        luminance > 0.5
+          ? chroma(colors[i]).darken(2)
+          : chroma(colors[i]).brighten(2);
+
+      items[i].style.color =
+        luminance > 0.5
+          ? chroma(colors[i]).brighten(1)
+          : chroma(colors[i]).darken(1);
+    });
+
+    items[i].addEventListener('mouseout', () => {
+      items[i].style.backgroundColor = currentBgColor;
+      items[i].style.color = currentItemColor;
+    });
+  }
+};
+
+setColumnsColor();
+setHoverProperties(hoverText);
+setHoverProperties(hoverButtons);
